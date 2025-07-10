a9b7a0232a651eebe44faf7e1c4f1ae8f535943d
ritikasharma78 <bewithritika17@gmail.com>
2025-07-10 13:49:05 +0530

    sidebar icons

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,14 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
 import useGlobalStore from "@/state/GlobalState";
-import {
-  LayoutDashboard,
-  Calendar,
-  Users,
-  BookOpen,
-  BarChart,
-  HelpCircle,
-  CircleEllipsis,
-} from "lucide-react";
 import Logo from "/images/logo/logo.webp";
 import { Divider } from "@nextui-org/react";
 
@@ -17,19 +8,38 @@ export default function Sidebar() {
   const location = useLocation();
 
   const navItems = [
-    { name: "Dashboard", path: "/", icon: LayoutDashboard },
-    { name: "Events", path: "/events", icon: Calendar },
-    { name: "Partners", path: "/partners", icon: Users },
-    { name: "Blogs", path: "/blogs", icon: BookOpen },
-    { name: "Community", path: "/community", icon: BookOpen },
+    { name: "Dashboard", path: "/", icon: "/banner/icons/Dashboard.svg" },
+    { name: "Events", path: "/events", icon: "/banner/icons/Events.svg" },
+    { name: "Partners", path: "/partners", icon: "/banner/icons/Partners.svg" },
+    { name: "Blogs", path: "/blogs", icon: "/banner/icons/Blogs.svg" },
+    {
+      name: "Community",
+      path: "/community",
+      icon: "/banner/icons/Community.svg",
+    },
     {
       name: "Submissions",
       path: "/submissions",
-      icon: BookOpen,
+      icon: "/banner/icons/Submission.svg",
     },
-    { name: "Analytics", path: "/analytics", icon: BarChart, disabled: true },
-    { name: "Support", path: "/support", icon: HelpCircle, disabled: true },
-    { name: "Others", path: "/others", icon: CircleEllipsis, disabled: true },
+    {
+      name: "Analytics",
+      path: "/analytics",
+      icon: "/banner/icons/Analytics.svg",
+      disabled: true,
+    },
+    {
+      name: "Support",
+      path: "/support",
+      icon: "/banner/icons/Support.svg",
+      disabled: true,
+    },
+    {
+      name: "Others",
+      path: "/others",
+      icon: "/banner/icons/Others.svg",
+      disabled: true,
+    },
   ];
 
   const isActive = (itemPath: string) => {
@@ -38,9 +48,11 @@ export default function Sidebar() {
     }
     return location.pathname.startsWith(itemPath);
   };
+
   const isProfile = location.pathname.startsWith("/profile");
+
   return (
-    <aside className="w-64 h-full  bg-white border-r border-gray-200 flex flex-col justify-between py-[12px]">
+    <aside className="w-64 h-full bg-white border-r border-gray-200 flex flex-col justify-between py-[12px]">
       <div>
         {/* Logo */}
         <div className="mb-1 flex justify-center items-center">
@@ -51,22 +63,22 @@ export default function Sidebar() {
 
         {/* Navigation */}
         <nav className="flex flex-col gap-1 px-2">
-          {navItems.map(({ name, path, icon: Icon, disabled }) => {
+          {navItems.map(({ name, path, icon, disabled }) => {
             const active = isActive(path);
             return (
               <div
                 key={name}
                 className={`flex items-center gap-3 rounded-lg px-3 py-2 text-sm font-medium transition-all duration-200
-        ${disabled ? "cursor-not-allowed text-gray-400" : active ? "bg-blue-50 text-blue-600" : "text-gray-700 hover:bg-blue-50 hover:text-blue-600"}`}
+                  ${disabled ? "cursor-not-allowed text-gray-400" : active ? "bg-[#CFEAFD] text-[#1098F7]" : "text-gray-700 hover:bg-blue-50 hover:text-blue-600"}`}
               >
                 {disabled ? (
                   <>
-                    <Icon size={18} />
+                    <img src={icon} alt={name} className="w-5 h-5" />
                     {name}
                   </>
                 ) : (
                   <Link to={path} className="flex items-center gap-3 w-full">
-                    <Icon size={18} />
+                    <img src={icon} alt={name} className="w-5 h-5" />
                     {name}
                   </Link>
                 )}
@@ -81,7 +93,7 @@ export default function Sidebar() {
         to={"/profile"}
         className={`flex items-center gap-3 rounded-lg px-3 py-2 mx-1 text-sm font-medium transition-all duration-200 ${
           isProfile
-            ? "bg-[#CFEAFD] text-black"
+            ? "bg-[#CFEAFD] text-[#526581]"
             : "text-gray-700 hover:bg-[#CFEAFD] border-t"
         }`}
       >
